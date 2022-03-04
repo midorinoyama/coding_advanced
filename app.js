9a3bdb804483038481a3d80895c6886ae7b3cd93
midorinomori <fujiaqa+geek@gmail.com>
2022-03-04 22:47:56 +0900

    [Add]slick

---
+++ app.js
@@ -0,0 +1,9 @@
+$('.slider').slick({
+  autoplay: true, // 自動再生
+  arrows: true, //矢印
+  dots: true, // ドットインジケーター表示
+  infinite: true, //スライドループ
+  slidesToShow: 2, //表示するスライド数
+  slidesToScroll: 1 //スクロールするスライド数
+
+});
